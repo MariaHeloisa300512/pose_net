ad0c286dd65be92c5d620ed4249561e04d3ad4cf
Professora Tábata Macedo <106038097+TabMacedo1897@users.noreply.github.com>
2023-06-12 17:44:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+let clownNose;
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    clownNose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.position(600, 250);
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+  clownNose.resize(100, 100, p5.prototype.AUTO);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+  
+}
+
+function modelLoaded() {
+  console.log('PoseNet foi inicializado');
+}
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+    noseX = results[0].pose.nose.x - 15;
+    noseY = results[0].pose.nose.y - 15;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clownNose, noseX, noseY, 30, 30);
+}
+
+function takeSnapshot() {
+  save('myFilterImage.png');
+}
+
+
